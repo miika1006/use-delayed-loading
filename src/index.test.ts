257447dac8ac98eb866eb56da3f13d0886efb729
miika1006 <18673835+miika1006@users.noreply.github.com>
2023-10-01 07:40:00 +0300

    Updated test to work with new packages

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,48 +1,38 @@
-import { useDelayedLoading } from './'
-import { renderHook, act } from "@testing-library/react-hooks";
+import { useDelayedLoading } from "./";
+import { act, renderHook } from "@testing-library/react-hooks";
 
-// mock timer using jest
 jest.useFakeTimers();
 
-describe('useDelayedLoading', () => {
- // it('should set loading true after delay', () => {
+describe("useDelayedLoading", () => {
+	test("Should have initial value of false, and after timeout change to true", () => {
+		const { result } = renderHook(() => {
+			const [loading, setLoading] = useDelayedLoading(false, 500);
+			console.log("RenderHook loading", loading);
+			return { loading, setLoading };
+		});
 
-    const { result } = renderHook(() => {
-      const [loading, setLoading] = useDelayedLoading(false, 500);
-      console.log("RenderHook loading",loading);
-      return { loading, setLoading };
-    });
+		expect(result.current.loading).toBe(false);
 
-    test('Should have initial value of false', () => {
-      expect(result.current.loading).toBe(false);
-      act(() => {
-        result.current.setLoading(true);
-      });
-    });
+		act(() => {
+			result.current.setLoading(true);
+		});
 
-    test('Should still be false after no delay', () => {
-      expect(result.current.loading).toBe(false);
-      // Fast-forward 600,  > 500
-      act(() => {
-        jest.advanceTimersByTime(600);
-      });
-    });
+		expect(result.current.loading).toBe(false);
 
-    test('Should now be true after delay', () => {
+		act(() => {
+			// Fast-forward 600,  > 500
+			jest.advanceTimersByTime(600);
+		});
 
-      // Check loading, should now be true
-      expect(result.current.loading).toBe(true);
-      
-      act(() => {
-        result.current.setLoading(false);
-        jest.advanceTimersByTime(1);
-      });
-    });
+		// Check loading, should now be true
+		expect(result.current.loading).toBe(true);
 
-    
-    test('Should immediatelly be false after no delay', () => {
-      //Should immediatelly be false
-      expect(result.current.loading).toBe(false);
-    });
-  //})
-})
+		act(() => {
+			result.current.setLoading(false);
+			jest.advanceTimersByTime(1);
+		});
+
+		//Should immediatelly be false
+		expect(result.current.loading).toBe(false);
+	});
+});
